ed0605486c8da5c5b6de6a3e63cd94ef171d1108
Andrew Trevisani <trevisani78@gmail.com>
2018-09-30 09:24:35 -0400

    submission

--- sw.js
+++ sw.js
@@ -1,12 +1,7 @@
-/* sources from https://developers.google.com/web/fundamentals/codelabs/offline/ 
-and https://developers.google.com/web/fundamentals/primers/service-workers/ 
-*/
 
-var CACHE_NAME = 'v1';
-var urlsToCache = [
-     './',
-    './index.html',
-    './restaurant.html',
+var cacheName = 'MyAppv1';
+var filesToCache = [
+    './',
     './css/styles.css',
     './data/restaurants.json',
     './img/1.jpg',
@@ -22,71 +17,28 @@ var urlsToCache = [
     './js/dbhelper.js',
     './js/main.js',
     './js/restaurant_info.js',
-    './sw.js'
+    './sw.js',
+    './index.html',
+    './restaurant.html'
+
 ];
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-
-        var fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest).then(
-          function(response) {
-            // Check if we received a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            var responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
-      })
-    );
-});
-
-// Delete caches that arent defined
-
-self.addEventListener('activate', function(event) {
-
-  var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
-
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching');
+      return cache.addAll(filesToCache);
     })
   );
 });
 
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
